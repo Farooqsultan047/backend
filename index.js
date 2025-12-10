6c3c2152b087ee7422d2e46b6f2c4feda789bae6
Farooqsultan047 <farooqsultan047@gmail.com>
2025-12-10 20:11:05 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+app.get('/twitter',(req,res)=>{
+    res.send('farooq sultan')
+})
+app.get('/login', (req, res) => {
+  res.send('<h1> Please login at Chai or Code </h1>');
+});
+app.get('/youtube', (req,res) => {
+    res.send('<h2> Awan group </h2>')
+    })
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${port}`)
+})
