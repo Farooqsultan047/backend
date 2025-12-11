f015a2a0c573fa2990db3e209e94cae0aa231d79
Farooqsultan047 <farooqsultan047@gmail.com>
2025-12-11 15:36:56 +0500

    github data added

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 4000
+
+// .env me PORT na ho to 4000 use karo
+const port = process.env.PORT || 4000
+
+const githubdata = 
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
-app.get('/twitter',(req,res)=>{
+
+app.get('/twitter', (req, res) => {
     res.send('farooq sultan')
 })
+
 app.get('/login', (req, res) => {
-  res.send('<h1> Please login at Chai or Code </h1>');
-});
-app.get('/youtube', (req,res) => {
-    res.send('<h2> Awan group </h2>')
-    })
-app.listen(process.env.PORT, () => {
-    console.log(`Example app listening on port ${port}`)
+    res.send('<h1>Please login at Chai or Code</h1>')
+})
+
+app.get('/youtube', (req, res) => {
+    res.send('<h2>Awan group</h2>')
+})
+
+app.get('/github', (req, res) => {
+    res.json(githubdata)
+})
+
+// Using correct port
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
 })
